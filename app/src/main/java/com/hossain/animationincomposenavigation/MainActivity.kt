872e934565ff91a5092378febcc83c6367646a8e
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-01-05 23:24:10 +0600

    initial commit

---
+++ app/src/main/java/com/hossain/animationincomposenavigation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hossain.animationincomposenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hossain.animationincomposenavigation.ui.theme.AnimationInComposeNavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationInComposeNavigationTheme {
+
+            }
+        }
+    }
+}
