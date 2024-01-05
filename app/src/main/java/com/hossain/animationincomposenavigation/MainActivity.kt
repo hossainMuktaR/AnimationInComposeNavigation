e9a0461886d1064d5d84c34b56008a9383a6cbca
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-01-06 04:49:45 +0600

    added details screen navigation enter and exit transition

--- app/src/main/java/com/hossain/animationincomposenavigation/MainActivity.kt
+++ app/src/main/java/com/hossain/animationincomposenavigation/MainActivity.kt
@@ -3,6 +3,14 @@ package com.hossain.animationincomposenavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,11 +35,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) {
                     val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "home_screen") {
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home_screen",
+                        enterTransition = { EnterTransition.None},
+                        exitTransition = { ExitTransition.None}
+                    ) {
                         composable(route = "home_screen") {
                             HomeScreen(navController)
                         }
-                        composable(route = "details_screen") {
+                        composable(
+                            route = "details_screen",
+                            enterTransition = {
+                                fadeIn(
+                                    animationSpec = tween(
+                                        500, easing = LinearEasing
+                                    )
+                                ) + slideIntoContainer(
+                                    animationSpec = tween(500, easing =  LinearEasing),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                                )
+                            },
+                            exitTransition = {
+                                fadeOut(
+                                    animationSpec = tween(
+                                        500, easing = LinearEasing
+                                    )
+                                ) + slideOutOfContainer(
+                                    animationSpec = tween(500, easing = EaseOut),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                                )
+                            }
+                        ) {
                             DetailsScreen(navController)
                         }
                         composable(route = "floatingA_screen") {
