e6389ac25ea011adbe0cd0b752c7e8281b96e79e
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-01-07 23:56:48 +0600

    added shared element between screen with compose on details screen

--- app/src/main/java/com/hossain/animationincomposenavigation/presentation/DetailsScreen.kt
+++ app/src/main/java/com/hossain/animationincomposenavigation/presentation/DetailsScreen.kt
@@ -1,8 +1,26 @@
 package com.hossain.animationincomposenavigation.presentation
 
 import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateInt
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,16 +32,34 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.hossain.animationincomposenavigation.R
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DetailsScreen(
     navController: NavController
 ) {
+    var profileScreen by remember {
+        mutableStateOf(false)
+    }
+    BackHandler {
+        backPress(
+            profileScreen,
+            { profileScreen = false },
+            { navController.popBackStack() }
+        )
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -32,11 +68,15 @@ fun DetailsScreen(
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
-                    Text("Details Screen")
+                    Text(if (profileScreen) "Profile Screen" else "Details Screen")
                 },
                 navigationIcon = {
                     IconButton(onClick = {
-                        navController.popBackStack()
+                        backPress(
+                            profileScreen,
+                            { profileScreen = false },
+                            { navController.popBackStack() }
+                        )
                     }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
@@ -47,9 +87,70 @@ fun DetailsScreen(
             )
         },
         modifier = Modifier.fillMaxSize()
-    ) {
-        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-            Text(text = "Details Screen")
+    ) { paddingValues ->
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(paddingValues).fillMaxSize()) {
+            val transition =
+                updateTransition(targetState = profileScreen, label = "image transition")
+            val size by transition.animateDp(
+                label = "image size",
+                transitionSpec = {
+                    tween(
+                        durationMillis = 500,
+                        easing = LinearOutSlowInEasing
+                    )
+                }
+            ) {
+                if (it) 400.dp else 100.dp
+            }
+            val radius by transition.animateInt(
+                label = "image radius",
+                transitionSpec = {
+                    tween(
+                        durationMillis = 500,
+                        easing = LinearOutSlowInEasing
+                    )
+                }
+            ) {
+                if (it) 0 else 50
+            }
+            val offsetY by transition.animateDp(
+                label = "image offset",
+                transitionSpec = {
+                    tween(
+                        durationMillis = 500,
+                        easing = LinearOutSlowInEasing
+                    )
+                }
+            ) {
+                if (it) 0.dp else (-250).dp
+            }
+            Column(modifier = Modifier.padding(if(profileScreen) 0.dp else 16.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.bird_image),
+                    contentDescription = "Bird Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(size)
+                        .clip(RoundedCornerShape(radius))
+                        .clickable {
+                            profileScreen = !profileScreen
+                        }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                AnimatedVisibility(
+                    visible = !profileScreen
+                ) {
+                    Text("The Yellow Bird")
+                }
+            }
         }
+    }
+}
+
+private fun backPress(state: Boolean, trueCallback: () -> Unit, falseCallback: () -> Unit) {
+    if (state) {
+        trueCallback()
+    } else {
+        falseCallback()
     }
 }
